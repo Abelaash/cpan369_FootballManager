53b8a2e1451760f1a11691697831985cc79f8bea
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2025-04-05 10:32:15 -0400

    Updated Accounts model

--- Models/Account.cs
+++ Models/Account.cs
@@ -14,23 +14,28 @@ namespace FootballManager.Models
         public int userId { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
 
         [Required]
         [EmailAddress]
+        [StringLength(50)]
         public string Email { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Username { get; set; }
 
         [Required]
+        [StringLength(255)]
         public string Password { get; set; }
 
         [NotMapped]
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
